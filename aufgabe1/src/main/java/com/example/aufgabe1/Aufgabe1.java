c8592dc5ea77b72af6d1c5d569071eaec6baada6
AnasKarazon <anaskarzoun@gmail.com>
2021-10-21 17:38:38 +0200

    handling plus button

--- aufgabe1/src/main/java/com/example/aufgabe1/Aufgabe1.java
+++ aufgabe1/src/main/java/com/example/aufgabe1/Aufgabe1.java
@@ -1,6 +1,8 @@
 package com.example.aufgabe1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -37,35 +39,54 @@ public class Aufgabe1 extends Application {
             0.0, 0.0,
             40.0, 0.0,
             20.0, 40.0 };
+    private Shape selectedElement;
+    private Pane anchorPaneKarte = new AnchorPane(this.getImage("berlin.png"));
+    private Pane kartenBox =  this.createVBoxWithAnchor(
+            0,
+            -1,
+            100d,
+            -1,
+            50d,
+            Pos.TOP_LEFT,
+            this.anchorPaneKarte
+    );
 
     @Override
     public void start(Stage stage) throws Exception {
+        this.selectedElement = null;
 
         // Museum
+        Shape museum = this.createRectangle(10, 10, 40, 40, true);
+
         Pane museumVBox = this.createVBox(
                 10,
                 Pos.CENTER,
-                this.createRectangle(10, 10, 40, 40),
+                museum,
                 this.createLabel("Museum")
         );
         // Info
+        Shape info = this.createCircle(20, true);
         Pane infoVBox = this.createVBox(
                 10,
                 Pos.CENTER,
-                this.createCircle(20),
+                info,
                 this.createLabel("Info")
         );
+
         // Sehenswürdigkeit
+        Shape sehenswuerdigkeit = this.createStar(true);
         Pane sehenswuerdigkeitVBox = this.createVBox(
                 10,
                 Pos.CENTER,
-                this.createStar(),
+                sehenswuerdigkeit,
                 this.createLabel("Sehenswürdigkeit"));
+
         // Restaurant
+        Shape restaurant = this.createTriangle(true);
         Pane restaurantVBox = this.createVBox(
                 10,
                 Pos.CENTER,
-                this.createTriangle(),
+                restaurant,
                 this.createLabel("Restaurant")
         );
 
@@ -85,6 +106,41 @@ public class Aufgabe1 extends Application {
         );
 
         // Editor-Box
+        ButtonBase plusBtn = this.createButton("+", 100);
+        ButtonBase minusBtn = this.createButton("-", 100);
+
+        EventHandler<ActionEvent> bKlick = new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if (selectedElement instanceof Rectangle) {
+                    Rectangle rectangle = (Rectangle) selectedElement;
+//                    AnchorPane.setLeftAnchor(rectangle, 10d);
+//                    AnchorPane.setTopAnchor(rectangle, 10d);
+                    anchorPaneKarte.getChildren().add(new Rectangle(rectangle.getHeight(), rectangle.getHeight()));
+                }
+                else if (selectedElement instanceof Circle) {
+                    Circle circle = (Circle) selectedElement;
+
+                    Circle newCircle = new Circle(circle.getRadius());
+                    AnchorPane.setLeftAnchor(newCircle, 10d);
+                    AnchorPane.setTopAnchor(newCircle, 10d);
+
+                    anchorPaneKarte.getChildren().add(newCircle);
+                }
+                else if (selectedElement instanceof Polygon) {
+                    Polygon polygon = (Polygon) selectedElement;
+
+                    Polygon newPolygon = new Polygon();
+                    newPolygon.getPoints().addAll(polygon.getPoints());
+                    newPolygon.setRotate(polygon.getRotate());
+
+                    anchorPaneKarte.getChildren().add(newPolygon);
+                }
+            }
+        };
+        plusBtn.addEventHandler(ActionEvent.ACTION, bKlick);
+
         Pane editorBox = this.createVBoxWithAnchor(
                 10,
                 -1 ,
@@ -92,23 +148,24 @@ public class Aufgabe1 extends Application {
                 210d,
                 -1,
                 Pos.TOP_LEFT,
-                this.createButton("+", 100),
-                this.createButton("-", 100)
+                plusBtn,
+                minusBtn
         );
 
         // Karten-Box
-        Pane kartenBox = this.createVBoxWithAnchor(
-                0,
-                -1,
-                100d,
-                -1,
-                50d,
-                Pos.TOP_LEFT,
-                this.getImage("berlin.png")
-        );
         kartenBox.setStyle("-fx-border-style: solid; -fx-border-color: black; -fx-border-width: 2");
 
         this.prepareScene(stage, linkeBox, editorBox, kartenBox);
+    }
+
+    private void changeSelectedElement(Shape geklicktesElement)
+    {
+        if (this.selectedElement != null) {
+            this.selectedElement.setFill(Color.BLACK);
+        }
+
+        geklicktesElement.setFill(Color.GREEN);
+        this.selectedElement = geklicktesElement;
     }
 
     public static void main(String[] args) {
@@ -126,9 +183,12 @@ public class Aufgabe1 extends Application {
      * @param diameter Durchmesser
      * @return Circle-Objekt
      */
-    private Shape createCircle(int diameter)
+    private Shape createCircle(int diameter, boolean highlightable)
     {
-        return new Circle(diameter);
+        Shape circle = new Circle(diameter);
+        if (highlightable)
+            circle.setOnMouseClicked(e -> changeSelectedElement(circle));
+        return circle;
     }
 
     /**
@@ -139,9 +199,12 @@ public class Aufgabe1 extends Application {
      * @param height Höhe
      * @return Erstelltes Rechteck-Shape
      */
-    private Shape createRectangle(int marginLeft, int marginTop, int width, int height)
+    private Shape createRectangle(double marginLeft, double marginTop, double width, double height, boolean highlightable)
     {
-        return new Rectangle(marginLeft, marginTop, width, height);
+        Shape rectangle = new Rectangle(marginLeft, marginTop, width, height);
+        if (highlightable)
+            rectangle.setOnMouseClicked(e -> changeSelectedElement(rectangle));
+        return rectangle;
     }
 
     /**
@@ -149,11 +212,12 @@ public class Aufgabe1 extends Application {
      *
      * @return Erstelle Stern-Shape
      */
-    private Shape createStar()
+    private Shape createStar(boolean highlightable)
     {
         Polygon starShape = new Polygon();
         starShape.getPoints().addAll(STERN);
-        starShape.setFill(Color.AQUAMARINE);
+        if (highlightable)
+            starShape.setOnMouseClicked(e -> changeSelectedElement(starShape));
 
         return starShape;
     }
@@ -163,11 +227,14 @@ public class Aufgabe1 extends Application {
      *
      * @return Erstelltes Dreieck
      */
-    private Shape createTriangle()
+    private Shape createTriangle(boolean highlightable)
     {
         Polygon triangle = new Polygon();
         triangle.getPoints().addAll(DREIECK);
         triangle.setRotate(180);
+
+        if (highlightable)
+            triangle.setOnMouseClicked(e -> changeSelectedElement(triangle));
 
         return triangle;
     }
@@ -228,8 +295,8 @@ public class Aufgabe1 extends Application {
 
     /**
      *
-     * @param stage
-     * @param items
+     * @param stage muss übergeben werden
+     * @param items die in das neue Pane hinzugefügt werden müssen
      * Erstellen der Scene und Vorbereiten & Anzeigen der Stage.
      */
     private void prepareScene(Stage stage, Node... items) {
