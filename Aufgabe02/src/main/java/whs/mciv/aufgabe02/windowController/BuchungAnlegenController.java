69126f88e3110e8bc37bcbb4f9dd62209540812c
Enes <support@eneserk.de>
2021-12-09 11:02:34 +0100

    BuchungAnlegen: Speichere das Objekt und gebe modifizierten Text als Label aus

--- Aufgabe02/src/main/java/whs/mciv/aufgabe02/windowController/BuchungAnlegenController.java
+++ Aufgabe02/src/main/java/whs/mciv/aufgabe02/windowController/BuchungAnlegenController.java
@@ -1,14 +1,16 @@
 package whs.mciv.aufgabe02.windowController;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.util.StringConverter;
 import whs.mciv.aufgabe02.BaseController;
 import whs.mciv.aufgabe02.daten.buchung.Buchung;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.stream.Collectors;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -23,7 +25,7 @@ public class BuchungAnlegenController extends BaseController {
     private TextField id;
 
     @FXML
-    private ComboBox kunde;
+    private ComboBox<Kunde> kundeComboBox;
 
     @FXML
     private TextField buchungsStatus;
@@ -38,10 +40,10 @@ public class BuchungAnlegenController extends BaseController {
     private TextField anzahlDerNaechte;
 
     @FXML
-    private ComboBox reiseziel;
+    private ComboBox<Reiseziel> reisezielComboBox;
 
     @FXML
-    private ComboBox verpflegung;
+    private ComboBox<String> verpflegung;
 
     @FXML
     private TextField gesamtpreis;
@@ -57,27 +59,10 @@ public class BuchungAnlegenController extends BaseController {
         // Erzeige neues Buchung-Objekt um die Daten des Formulars später
         // speichern zu können. Der Buchung-Konstruktor erzeugt auch die ID.
         buchung = new Buchung();
-        HashMap<String, Reiseziel> reisezieleData = ReisezielDaten.getReiseziele();
-        HashMap<String, Kunde> kundenData = KundenDaten.getKunden();
-
-        for (Reiseziel ziel: reisezieleData.values()) {
-            if (ziel != null && ziel.getName() != null) {
-                this.reisezielName.add(ziel.getName());
-            }
-        }
-        Collections.sort(reisezielName, String.CASE_INSENSITIVE_ORDER);
-        this.reiseziel.getItems().setAll(reisezielName);
-
-
-        for (Kunde kundeDatensatz: kundenData.values()) {
-            if (kundeDatensatz != null && kundeDatensatz.getNachname() != null && kundeDatensatz.getVorname() != null) {
-                this.kundenNamen.add(kundeDatensatz.getNachname() + ", " + kundeDatensatz.getVorname() + " (" + kundeDatensatz.getId() + ")");
-            }
-        }
-        Collections.sort(kundenNamen, String.CASE_INSENSITIVE_ORDER);
-        this.kunde.getItems().setAll(kundenNamen);
-
         this.id.setText(buchung.getId());
+
+        this.setzeReiseziele();
+        this.setzeKundenDaten();
 
         // Setze Verpflegung
 //        List<String> verpflegungAuswahl = new ArrayList<String>();
@@ -85,6 +70,48 @@ public class BuchungAnlegenController extends BaseController {
 //        verpflegungAuswahl.add("Vollpension");
 //        ObservableList obList = FXCollections.observableList(verpflegungAuswahl);
 //        this.verpflegung.setItems(obList);
+    }
+
+    private void setzeKundenDaten() {
+        ObservableList<Kunde> kundenDaten = FXCollections.observableArrayList();
+        Collection<Kunde> alleKunden = KundenDaten.getAllKunden();
+        List<Kunde> alleKundenSortiert = alleKunden.stream().sorted(Comparator.comparing(Kunde::getNachname)).collect(Collectors.toList());
+
+        kundenDaten.addAll(alleKundenSortiert);
+
+        this.kundeComboBox.setItems(kundenDaten);
+        this.kundeComboBox.setConverter(new StringConverter<Kunde>() {
+            @Override
+            public String toString(Kunde kundenDatensatz) {
+                return kundenDatensatz.getNachname() + ", " + kundenDatensatz.getVorname() + " (" + kundenDatensatz.getId() + ")";
+            }
+
+            @Override
+            public Kunde fromString(String string) {
+                return kundeComboBox.getItems().stream().filter(kunde -> kunde.getId().equals(string)).findFirst().orElse(null);
+            }
+        });
+    }
+
+    private void setzeReiseziele() {
+        ObservableList<Reiseziel> reisezielDaten = FXCollections.observableArrayList();
+        Collection<Reiseziel> alleReiseziele = ReisezielDaten.getAllReiseziele();
+        List<Reiseziel> alleReisezieleSortiert = alleReiseziele.stream().sorted(Comparator.comparing(Reiseziel::getName)).collect(Collectors.toList());
+
+        reisezielDaten.addAll(alleReisezieleSortiert);
+
+        this.reisezielComboBox.setItems(reisezielDaten);
+        this.reisezielComboBox.setConverter(new StringConverter<Reiseziel>() {
+            @Override
+            public String toString(Reiseziel reisezielDatensatz) {
+                return reisezielDatensatz.getName() + " (" + reisezielDatensatz.getId() + ")";
+            }
+
+            @Override
+            public Reiseziel fromString(String string) {
+                return reisezielComboBox.getItems().stream().filter(reiseziel -> reiseziel.getId().equals(string)).findFirst().orElse(null);
+            }
+        });
     }
 
     public void setStage(Stage stage) {
