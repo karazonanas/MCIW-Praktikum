194c79f4154d2c125e2ab7d417351c5f465d98e6
Enes <support@eneserk.de>
2022-01-12 19:19:02 +0100

    fix: Passe IBAN-Filter an

--- Aufgabe03/src/main/java/whs/mciv/aufgabe02/filter/FilterIban.java
+++ Aufgabe03/src/main/java/whs/mciv/aufgabe02/filter/FilterIban.java
@@ -6,14 +6,19 @@ import java.awt.*;
 import java.util.function.UnaryOperator;
 
 public class FilterIban implements UnaryOperator<TextFormatter.Change> {
-    public static final String ibanRegex = "^($|[A-Z]{0,2})\\d{0,20}";
+    // Regex für laufende Prüfung
+    public static final String ibanRegex = "[A-Z]{0,2}\\d{0,2}\\s?(?:\\d{0,4}\\s?){0,3}\\d{0,4}(?:\\s?\\d{0,2})";
+    // Regex für schlussendliche Prüfung
+    public static final String ibanRegexFinal = "[A-Z]{2}\\d{2}\\s?(?:\\d{4}\\s?){3}\\d{4}(?:\\s?\\d{2})";
+
     @Override
     public TextFormatter.Change apply(TextFormatter.Change change) {
             String neu = change.getControlNewText();
+
             if (! neu.matches(ibanRegex)) {
                 Toolkit.getDefaultToolkit().beep();
                 return null;
             }
             return change;
-        }
+    }
 }
